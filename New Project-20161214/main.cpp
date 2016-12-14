7a9fef50efa694a3224613db6e430ea63e57402c
zalbertz <catalap2002@gmail.com>
2016-12-14 13:45:41 +0200

    Created by Coding Ground Client

---
+++ New Project-20161214/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int x;
+   int y;
+  x = 5;
+  y = 13;
+  cout << "+ + + + + + + + + + + + + + + + +" << endl;
+  cout << "*********************************" << endl;
+  cout << "* Apsveicu " << y << "." << " dzimsanas dienaa *" << endl;
+  cout << "*         " << x << " novembris           *" << endl;
+  cout << "*   *                           *" << endl;
+  cout << "*  ***                          *" << endl;
+  cout << "* *****                         *" << endl;
+  cout << "*   *                 Alberts Z *" << endl;
+  cout << "*********************************" << endl;  
+   return 0;
+}
+
